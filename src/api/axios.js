d28c10de007d27f82c7203f80f96b320cadabd7f
afurkanbicak <afurkanbicak@gmail.com>
2022-05-27 19:43:44 +0300

    baseUrl ve endpointler eklendi

---
+++ src/api/axios.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+export const baseURL = 'https://dev-smoothie-api.fintechyazilim.com'
+export default axios.create({ baseURL })
+
+export const URL = {
+    login           :   '/api/User/Login',
+    forgotPassword  :   '/api/User/ForgotPassword',
+    changePassword  :   '/api/User/ChangePassword',
+    customer        :   '/api/v1/FinTech/Execute',
+
+}
