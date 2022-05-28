baf30db6e9cfd239ff434b9b887bb82b9f541630
afurkanbicak <afurkanbicak@gmail.com>
2022-05-28 14:38:29 +0300

    Müşteri id bilgisine göre hesaplarını getiren servis ya eklendi

--- src/services/customerGetServices.js
+++ src/services/customerGetServices.js
@@ -26,3 +26,29 @@ export const getCustomer = async () => {
         console.log("error")
     }
 }
+
+//? Müşterilerin id bilgisine göre hesaplarını getiren servis.
+export const getCustomerAccount = async (id) => {
+    formData.append('Action', 'GetAccountByCustomerId')
+    formData.append('Version', 1)
+    formData.append('Parameters', `{CustomerId: "${id}"}`)
+
+    try {
+        const res = await axios.post(URL.customer, formData, { 
+            'content-type': 'multipart/form-data' 
+        });
+        
+        if(res.status === 200){
+            console.log("Data", res.data)
+            return res.data
+        } 
+        else{
+            return {
+                error: 'Data gelmedi'
+            }
+        }
+       
+    } catch (error) {
+        console.log("error")
+    }
+}
