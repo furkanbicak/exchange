a3f9b4a993ab9d2e4ddf1b7fd87c8207139fa168
afurkanbicak <afurkanbicak@gmail.com>
2022-05-28 20:15:00 +0300

    Hesabım sayfası inşa edildi.

---
+++ src/pages/Account/Account.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ChangePassword from '../../components/changePassword'
+import NavbarContent from '../../components/NavbarContent'
+
+const Account = () => {
+  return (
+    <>
+      <NavbarContent />
+      <ChangePassword />
+    </>
+    
+  )
+}
+
+export default Account
