cf42898de12c4793c1b763027607c1db4fd237b5
afurkanbicak <afurkanbicak@gmail.com>
2022-05-28 20:13:21 +0300

    formData fonksiyon içinde const ile tanımlandı

--- src/services/customerGetServices.js
+++ src/services/customerGetServices.js
@@ -1,9 +1,9 @@
 import axios, { URL } from '../api/axios';
 
-let formData = new FormData()
-
 //? Müşterileri getiren servis.
 export const getCustomer = async () => {
+    const formData = new FormData()
+    
     formData.append('Action', 'GetCustomer')
     formData.append('Version', 1)
     formData.append('Parameters', '{}')
@@ -27,28 +27,3 @@ export const getCustomer = async () => {
     }
 }
 
-//? Müşterilerin id bilgisine göre hesaplarını getiren servis.
-export const getCustomerAccount = async (id) => {
-    formData.append('Action', 'GetAccountByCustomerId')
-    formData.append('Version', 1)
-    formData.append('Parameters', `{CustomerId: "${id}"}`)
-
-    try {
-        const res = await axios.post(URL.customer, formData, { 
-            'content-type': 'multipart/form-data' 
-        });
-        
-        if(res.status === 200){
-            console.log("Data", res.data)
-            return res.data
-        } 
-        else{
-            return {
-                error: 'Data gelmedi'
-            }
-        }
-       
-    } catch (error) {
-        console.log("error")
-    }
-}
