3e32fff2aaa2e66128beca1e3ae1cb48afc3fba5
afurkanbicak <afurkanbicak@gmail.com>
2022-05-30 02:01:58 +0300

    kontrol component inşa edildi

---
+++ src/components/RequireAuth.js
@@ -0,0 +1,19 @@
+import { useLocation, Navigate, Outlet } from "react-router-dom"
+import useAuth from "../hooks/useAuth"
+
+const RequireAuth = () => {
+    const { auth } = useAuth()
+    const location = useLocation()
+
+    console.log("Auth",auth)
+    
+    return (
+        auth?.accessToken
+            ? <Outlet />
+            : auth?.user
+                ? <Navigate to="/unauthorized" state={{ from: location }} replace />
+                : <Navigate to="/login" state={{ from: location }} replace />
+    );
+}
+
+export default RequireAuth;
