8cc8e0205a2e5d817dbf7e184dc2c55ccc04aec8
afurkanbicak <afurkanbicak@gmail.com>
2022-05-29 22:00:21 +0300

    Error sayfası inşa edildi.

---
+++ src/components/Error.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
+import ErrorIcon from '../assets/error.svg'
+
+const Error = () => {
+    const navigate = useNavigate()
+
+    
+    return (
+       <>
+       <div class="container">
+            <div class="row">
+                <div class="col d-flex justify-content-center">
+                    <img
+                        alt         =   'navbarIcon'
+                        src         =   { ErrorIcon }
+                        width       =   '700'
+                        height      =   '700'
+                        className   =   'd-inline-block align-top w-75'
+                    />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col d-flex justify-content-center">
+                    <Button className='w-25' onClick={() => navigate(-1)}>Go Back</Button>
+                </div>
+            </div>
+        </div>
+       </>
+    )
+}
+
+export default Error
