2def56506cd3cb4b83c9f5940475003314d95369
afurkanbicak <afurkanbicak@gmail.com>
2022-05-29 22:27:43 +0300

    router dosyası inşa edildi

--- src/router/router.js
+++ src/router/router.js
@@ -1,9 +1,38 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import '../App.css'
+import Login from '../pages/Login/login'
+import Home from '../pages/Home/home'
+import Layout from '../components/Layout'
+import RequireAuth from '../components/RequireAuth'
+import ForgotPassword from '../pages/ForgotPassword/forgotPassword'
+import Account from '../pages/Account/Account'
+import CustomerAccountList from '../pages/CustomerAccountList/customerAccountList'
+import ForgotPasswordChange from '../pages/ForgotPasswordChange/forgotPasswordChange'
+import Unauthorized from '../pages/Unauthorized/Unauthorized'
+
 
 const Router = () => {
-  return (
-    <div>Router</div>
-  )
+    return (
+        <>
+            <Routes>
+                {/* public routes */}
+                <Route path='/' element={<Layout />}>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/forgotPassword" element={<ForgotPassword />} />
+                    <Route path="/unauthorized" element={<Unauthorized />} />
+                </Route>
+                
+                {/* private routes */}
+                <Route element={<RequireAuth />}>
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/account" element={<Account />} />
+                    <Route path="/table-account-list/:CustomerId" element={<CustomerAccountList />} />
+                    <Route path="/ForgotPasswordChange" element={<ForgotPasswordChange />} />
+                </Route>
+            </Routes>
+        </>
+    )
 }
 
 export default Router
