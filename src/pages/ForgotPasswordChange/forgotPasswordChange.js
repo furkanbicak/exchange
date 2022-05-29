ecd3851d33a7896935d877431d09b27f08714646
afurkanbicak <afurkanbicak@gmail.com>
2022-05-30 01:10:41 +0300

    Maile gelen link ile şifre sıfırlama fonksiyonu.

---
+++ src/pages/ForgotPasswordChange/forgotPasswordChange.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import Swal from 'sweetalert2'
+import ForgotChangePasswordForm from '../../components/ForgotChangePasswordForm'
+import { forgotChangePassword } from '../../services/forgotChangePassword'
+
+
+const ForgotPasswordChange = () => {
+
+    //? Mailden gelen url'deki 'param' keyinin value değerini yakalar.
+    const urlParams = new URLSearchParams(window.location.search)
+    const param = urlParams.get('param')
+
+    
+    //? Şifre değiştirme fonksiyonu.
+    const changeToPassword = async (e) => {
+        const email = e.email
+        const password = e.password
+
+        const res = await forgotChangePassword(param, email, password)
+        console.log("Şifre Değişti:", res)
+
+
+        //? İşlem başarılı ise success alert.
+        if (res?.IsSuccess === true) {
+            Swal.fire({
+                position: 'top-end',
+                icon: 'success',
+                title: res?.Result.Message,
+                showConfirmButton: false,
+                timer: 1500
+            })
+        
+        } 
+        //? İşlem başarısız ise error alert.
+        else {
+            Swal.fire({
+                position: 'top-end',
+                icon: 'error',
+                title: res?.Result.Message,
+                showConfirmButton: false,
+                timer: 1500
+            })
+        }
+    }
+
+
+    return (
+        <>
+            <ForgotChangePasswordForm changeToPassword = { changeToPassword } />
+        </>
+    )
+}
+
+export default ForgotPasswordChange
