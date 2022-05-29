c957510ae14c8b5f820f9849dc334cc627fb9ce3
afurkanbicak <afurkanbicak@gmail.com>
2022-05-29 22:00:55 +0300

    navbar yönlendirme bug fix

--- src/components/NavbarContent.js
+++ src/components/NavbarContent.js
@@ -1,30 +1,35 @@
 import React from 'react'
 import { Container, Nav, Navbar } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
 import loginIcon from '../../src/assets/login.svg'
 
+
 const NavbarContent = () => {
-  return (
-    <>
-    <Navbar bg="primary" variant="dark">
-    <Container>
-    <Navbar.Brand href="/home">
-          <img
-            alt=""
-            src={loginIcon}
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
-          />
-        Fintech
-        </Navbar.Brand>
-    <Nav className="me-auto">
-        <Nav.Link href="/home">Home</Nav.Link>
-        <Nav.Link href="/account">Account</Nav.Link>
-    </Nav>
-    </Container>
-  </Navbar>
-  </>
-  )
+    const navigate = useNavigate();
+
+
+    return (
+        <>
+        <Navbar bg='primary' variant='dark'>
+            <Container>
+                <Navbar.Brand href='/home'>
+                    <img
+                        alt         =   'navbarIcon'
+                        src         =   { loginIcon }
+                        width       =   '50'
+                        height      =   '50'
+                        className   =   'd-inline-block align-top'
+                    />
+                    Fintech
+                </Navbar.Brand>
+                <Nav className='me-auto'>
+                    <Nav.Link className = 'text-white' onClick={ ()=>navigate('/home') }>Home</Nav.Link>
+                    <Nav.Link className = 'text-white' onClick={ ()=>navigate('/account') }>Account</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+        </>
+    )
 }
 
 export default NavbarContent
