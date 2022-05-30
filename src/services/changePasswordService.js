05da7344f10215eeaf4c6fc9943c7befc2b984ba
afurkanbicak <afurkanbicak@gmail.com>
2022-05-30 12:31:35 +0300

    code review

---
+++ src/services/changePasswordService.js
@@ -0,0 +1,33 @@
+import axios, { URL } from '../api/axios'
+
+
+//? Şifre değiştirme servisi.
+export const getChangePassword = async ( email, currentPassword, password ) => {
+    console.log("Email",email)
+    console.log("currentPassword",currentPassword)
+    console.log("password",password)
+
+    const formData = new FormData()
+   
+    formData.append('Email', `${email}`)
+    formData.append('CurrentPassword', `${currentPassword}`)
+    formData.append('Password', `${password}`)
+
+    try {
+        const res = await axios.post(URL.changePassword, formData, { 
+            'content-type': 'multipart/form-data' 
+        });
+        
+        if (res.status === 200) {
+            console.log("Data", res.data)
+            return res.data
+        } 
+        else {
+            return {
+                error: 'Data gelmedi'
+            }
+        }
+    } catch (error) {
+        console.log("error")
+    }
+}
