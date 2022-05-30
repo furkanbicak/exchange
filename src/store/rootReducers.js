7a04601b079eb05441d25691a54cd8285d314d00
afurkanbicak <afurkanbicak@gmail.com>
2022-05-30 06:11:00 +0300

    reducere inşa edildi

---
+++ src/store/rootReducers.js
@@ -0,0 +1,40 @@
+
+import {combineReducers} from 'redux'
+
+const initialState={
+    accesToken:'',
+}
+
+const initialUserState={
+    arr:[]
+}
+
+function rootReducer(state=initialState, action){
+    switch(action.type){
+            case 'SET_TOKEN':
+                return{
+                        accesToken:action.payload,
+                    }
+            default:
+                return state
+    }
+}
+
+function userReducer(state=initialUserState, action){
+    switch(action.type){
+            case 'SET_ARR':
+                return{
+                        ...state,
+                        arr:action.payload,
+                    }
+            default:
+                return state
+    }
+}
+
+const reducer = combineReducers({
+    accesToken:rootReducer,
+    arr:userReducer
+})
+
+export default reducer
