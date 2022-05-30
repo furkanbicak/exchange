cb07d4d59904e755798605be8ae06d4e8040cfc2
afurkanbicak <afurkanbicak@gmail.com>
2022-05-30 05:58:45 +0300

    component center bug-fix

--- src/components/ForgotChangePasswordForm.js
+++ src/components/ForgotChangePasswordForm.js
@@ -11,85 +11,87 @@ const ForgotChangePasswordForm = ({ changeToPassword }) => {
 
     return (
         <>
-            <Container>
-                <Row className = 'mt-5 shadow p-3 mb-5 bg-white rounded m-auto'>
-                    <Col lg={5} md={6} sm={12} className = 'p-5 m-auto'>
-                        <img 
-                            src         =   { loginIcon } 
-                            alt         =   'loginIcon.svg'
-                            className   =   'w-100 rounded mx-auto d-block mt-5' 
-                        />
-                    </Col>
-                    <Formik
-                        initialValues = {{
-                            email       :   '',
-                            password    :   ''
-                        }}
+            <div className="d-flex justify-content-center align-items-center" style={{height:'100vh'}}>
+                <Container>
+                    <Row className = 'mt-5 shadow p-3 mb-5 bg-white rounded m-auto'>
+                        <Col lg={5} md={6} sm={12} className = 'p-5 m-auto'>
+                                <img 
+                                    src         =   { loginIcon } 
+                                    alt         =   'loginIcon.svg'
+                                    className   =   'w-100 rounded mx-auto d-block mt-5' 
+                                />
+                        </Col>
+                        <Formik
+                            initialValues = {{
+                                email       :   '',
+                                password    :   ''
+                            }}
 
-                        validationSchema = { LoginSchema }
-                        
-                        onSubmit = { (values) => {
-                            changeToPassword(values)
-                        }}
-                    >
-                        {
-                            ( { values, errors, touched, handleSubmit, handleChange, handleBlur } ) => (
-                                <Col lg={5} md={6} sm={12} className = 'p-5 m-auto'>
-                                    <h1 className = 'my-5 font-weight-bold text-primary'>Change Password</h1>
-                                    <Form onSubmit = {handleSubmit} className = 'mb-5' >
-                                        
-                                        <Form.Group className = 'mt-5' controlId = 'formBasicEmail' >
-                                            <Form.Label>Email address</Form.Label>
-                                            <Form.Control 
-                                                type        =   'email' 
-                                                name        =   'email' 
-                                                placeholder =   'Enter email' 
-                                                onBlur      =   { handleBlur } 
-                                                onChange    =   { handleChange } 
-                                                value       =   { values.email }  
-                                            />
-                                                {
-                                                    errors.email && touched.email && (
-                                                        <Form.Label className = 'text-danger mt-2'>
-                                                            *{errors.email}
-                                                        </Form.Label>
-                                                    )
-                                                }
-                                        </Form.Group>
+                            validationSchema = { LoginSchema }
+                                    
+                            onSubmit = { (values) => {
+                                changeToPassword(values)
+                            }}
+                        >
+                            {
+                                ( { values, errors, touched, handleSubmit, handleChange, handleBlur } ) => (
+                                    <Col lg={5} md={6} sm={12} className = 'p-5 m-auto'>
+                                        <h1 className = 'my-5 font-weight-bold text-primary'>Change Password</h1>
+                                        <Form onSubmit = {handleSubmit} className = 'mb-5' >
+                                                    
+                                                    <Form.Group className = 'mt-5' controlId = 'formBasicEmail' >
+                                                        <Form.Label>Email address</Form.Label>
+                                                        <Form.Control 
+                                                            type        =   'email' 
+                                                            name        =   'email' 
+                                                            placeholder =   'Enter email' 
+                                                            onBlur      =   { handleBlur } 
+                                                            onChange    =   { handleChange } 
+                                                            value       =   { values.email }  
+                                                        />
+                                                            {
+                                                                errors.email && touched.email && (
+                                                                    <Form.Label className = 'text-danger mt-2'>
+                                                                        *{errors.email}
+                                                                    </Form.Label>
+                                                                )
+                                                            }
+                                                    </Form.Group>
 
-                                        <Form.Group className = 'mt-4' controlId = 'formBasicPassword'>
-                                            <Form.Label>Password</Form.Label>
-                                            <Form.Control 
-                                                type        =   'password' 
-                                                name        =   'password' 
-                                                placeholder =   'Enter Password' 
-                                                onBlur      =   { handleBlur } 
-                                                onChange    =   { handleChange }
-                                                value       =   { values.password }  
-                                            />
-                                                {
-                                                    errors.password && touched.password && (
-                                                        <Form.Label className='text-danger mt-2'>
-                                                            *{errors.password}
-                                                        </Form.Label>
-                                                    )
-                                                }
-                                        </Form.Group>
+                                                    <Form.Group className = 'mt-4' controlId = 'formBasicPassword'>
+                                                        <Form.Label>Password</Form.Label>
+                                                        <Form.Control 
+                                                            type        =   'password' 
+                                                            name        =   'password' 
+                                                            placeholder =   'Enter Password' 
+                                                            onBlur      =   { handleBlur } 
+                                                            onChange    =   { handleChange }
+                                                            value       =   { values.password }  
+                                                        />
+                                                            {
+                                                                errors.password && touched.password && (
+                                                                    <Form.Label className='text-danger mt-2'>
+                                                                        *{errors.password}
+                                                                    </Form.Label>
+                                                                )
+                                                            }
+                                                    </Form.Group>
 
-                                        <Button type = 'submit' variant = 'primary w-100 mt-5' onClick = {handleSubmit}>
-                                            Submit
-                                        </Button>
-                                        
-                                    </Form>
-                                </Col>
-                            )
-                        }
-                    </Formik>
-                </Row>
-                <h6 className = 'text-center text-secondary'>
-                    Copyright © 2022 Fintech. All Rights Reserved.
-                </h6>
-            </Container>
+                                                    <Button type = 'submit' variant = 'primary w-100 mt-5' onClick = {handleSubmit}>
+                                                        Submit
+                                                    </Button>
+                                                    
+                                        </Form>
+                                    </Col>
+                                )
+                            }
+                        </Formik>
+                    </Row>
+                    <h6 className = 'text-center text-secondary'>
+                        Copyright © 2022 Fintech. All Rights Reserved.
+                    </h6>
+                </Container>
+            </div>
         </>
     )
 }
