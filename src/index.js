2568816b9c29dcd93ed43237ff50b88f31e3f04e
afurkanbicak <afurkanbicak@gmail.com>
2022-05-30 05:57:33 +0300

    redux için store eklendi

--- src/index.js
+++ src/index.js
@@ -4,18 +4,24 @@ import { BrowserRouter, Routes, Route} from "react-router-dom";
 import './index.css';
 import App from './App';
 import { AuthProvider } from './context/AuthProvider';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from '../src/store/rootReducers';
+const store = createStore(reducer)
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <BrowserRouter>
-  <AuthProvider>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-  </AuthProvider>
-  </BrowserRouter>
+    <Provider store = { store }>
+      <BrowserRouter>
+        <AuthProvider>
+            <Routes>
+              <Route path="/*" element={<App />} />
+            </Routes>
+        </AuthProvider>
+      </BrowserRouter>
+    </Provider>
 </React.StrictMode>
 );
 
